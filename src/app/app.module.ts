98211f2b7e0ba8fba264e021a0c0a583fedf704d
KyleNBurke <kyle.n.burke@gmail.com>
2018-06-29 16:32:31 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { BrowseComponent } from './browse/browse.component';
+import { SearchComponent } from './search/search.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    PlaylistsComponent,
+    BrowseComponent,
+    SearchComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
