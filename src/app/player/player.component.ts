66e918bfe302144064ccb14553a778ca62eb7821
KyleNBurke <kyle.n.burke@gmail.com>
2018-07-30 10:20:22 -0500

    various improvements

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Track } from '../shared/track.model';
 import { PlayerService } from '../shared/player.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player',
@@ -9,12 +10,20 @@ import { PlayerService } from '../shared/player.service';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor(private playerService: PlayerService) { }
+  constructor(private playerService: PlayerService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onToggleSong() {
     this.playerService.playing ? this.playerService.pauseCurrentSong() : this.playerService.playCurrentSong();
+  }
+
+  onAlbumClick() {
+    this.router.navigate(['/album', this.playerService.track.albumID]);
+  }
+
+  onArtistClick(index: number) {
+    this.router.navigate(['/artist', this.playerService.track.artistID[index]]);
   }
 }
